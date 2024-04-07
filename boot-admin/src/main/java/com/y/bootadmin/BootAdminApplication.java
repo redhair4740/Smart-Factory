87a06fa9cbafcbfd345719424078ac68b91615a1
Y <yuejie.huang@weifu.com.cn>
2024-04-07 10:16:15 +0800

    初始化admin模块

---
+++ boot-admin/src/main/java/com/y/bootadmin/BootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.y.bootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAdminApplication.class, args);
+    }
+
+}
---
+++ boot-admin/src/test/java/com/y/bootadmin/BootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y.bootadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
