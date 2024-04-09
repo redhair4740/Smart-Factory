4fd96a72ec5692a554f6990cecea35e3102737c0
xiaopan <1054386461@qq.com>
2024-04-09 22:02:42 +0800

    feat: token控制添加

---
+++ vulcan-admin/src/main/java/com/vulcan/controller/TokenController.java
@@ -0,0 +1,16 @@
+package com.vulcan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pxj
+ * @Project: Smart-Factory
+ * @Package: com.vulcan.controller
+ * @name: token 控制
+ * @Date: 2024/4/9  下午9:45
+ * @Description token 控制
+ */
+@RestController
+public class TokenController {
+
+}
