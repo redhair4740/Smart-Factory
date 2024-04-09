ef6aa83b64db3ab50258540a7c61d27beb90d86e
yuejie.huang <HYJ628928>
2024-04-09 12:53:36 +0800

    feat: 调整项目名称，创建公告模块以及依赖模块

--- vulcan-admin/src/test/java/com/vulcan/BootAdminApplicationTests.java
+++ vulcan-admin/src/test/java/com/vulcan/BootAdminApplicationTests.java
@@ -1,4 +1,4 @@
-package com.y.bootadmin;
+package com.vulcan;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ vulcan-common/src/main/java/com/vulcan/annotation/Log.java
@@ -0,0 +1,37 @@
+package com.vulcan.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Y
+ * @Project: Smart-Factory
+ * @Package: com.vulcan.annotation
+ * @name: Log
+ * @Date: 2024/4/7  上午11:55
+ * @Description 自定义操作日志记录注解
+ */
+@Target({ ElementType.PARAMETER, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Log
+{
+    /**
+     * 模块
+     */
+    public String title() default "";
+
+    /**
+     * 是否保存请求的参数
+     */
+    public boolean isSaveRequestData() default true;
+
+    /**
+     * 是否保存响应的参数
+     */
+    public boolean isSaveResponseData() default true;
+
+    /**
+     * 排除指定的请求参数
+     */
+    public String[] excludeParamNames() default {};
+}
