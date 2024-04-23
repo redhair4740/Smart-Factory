c2183c2578650cb469a09c1ee9923cda8a49377f
Y <1783154732@qq.com>
2024-04-23 11:15:34 +0800

    feat: 添加图标库,axios,字体库

--- vulcan-ui/vite.config.js
+++ vulcan-ui/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
+// 自动引入插件
+import Components from 'unplugin-vue-components/vite'
+// Naiue UI解析器
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      Components({
+        dts: true, // ts 环境下要启用
+        resolvers: [NaiveUiResolver()],
+        dirs: ['src/components', 'src/layouts'], // 扫描的文件夹,引入自定义组件
+      })
+  ],
 })
